24e3a6be5f751ec9f16e486cdb0e9baf115b5fc1
Demchenko Evgeny <demchenko@inf.ai>
2018-04-26 17:38:55 +0400

    Split to submodules profiler and logger.

--- index.js
+++ index.js
@@ -1,15 +1,98 @@
+const {LoggerConsole} = require('@goldix.org/logger-console');
+const {LoggerProxy} = require('./utils/LoggerProxy');
 
-
-const { Loggers } = require('./loggers');
-const DefaultLogger = require('./loggers/default').Logger;
-const {LoggerProxy } = require('./utils/LoggerProxy');
-const {Profilers } = require('./profilers');
-const DefaultProfiler = require('./profilers/default').Profiler;
-
-module.exports = {
-  Loggers,
-  LoggerProxy,
-  DefaultLogger,
-  Profilers,
-  DefaultProfiler
+let loggerClasses = {
+  LoggerConsole
 };
+
+class LoggersPool extends  LoggerProxy {
+  
+  constructor(poolOptions, options) {
+    super(null, options);
+    this.poolOptions = {
+      ...poolOptions
+    };
+    this._loggers = [];
+    this.createLoggers();
+  }
+  
+  createLoggers() {
+    Object.keys(this.poolOptions).forEach(className => {
+      const loggerOptions = this.poolOptions[className];
+      if(loggerOptions) {
+        if(loggerOptions.enabled === false) return;
+        if(loggerOptions.disabled === true) return;
+      }
+      if(className in loggerClasses) {
+        this._loggers.push(
+          new loggerClasses[className](loggerOptions)
+        );
+      } else {
+        throw new Error(`Unknown logger ${className}. Check your config of logger pools or define more loggers by Loggers.define().`);
+      }
+    });
+  }
+  
+  _log(level, message, payload, options) {
+    let promises = [];
+    options = Object.freeze({
+      ...this.options,
+      options,
+      poolOptions: this.poolOptions
+    });
+    this.loggers.forEach(logger => {
+      promises.push(
+        logger._log(level, message, payload, options)
+      );
+    });
+    return Promise.all(promises);
+  }
+}
+
+class Loggers {
+  
+  /**
+   *
+   * @param {object} options
+   * @param {object} options.pools   {<poolName>: { <loggerClassName>: {<logger options>} }}
+   */
+  
+  constructor(options) {
+    this.options = {
+      pools: {
+        default: {
+          LoggerConsole: { level: 'info' },
+        },
+      },
+      ...options,
+    };
+    
+    this.instances = {};
+  }
+  
+  createPool(poolName) {
+    if(poolName in this.options.pools) {
+      return new LoggersPool(this.options.pools[poolName]);
+    }
+    
+    return this.instances['default']
+      || (this.instances['default'] = new LoggersPool(this.options.pools['default'], this.options));
+  }
+  
+  get(poolName, options) {
+    const logger = this.instances[poolName] || (this.instances[poolName] = this.createPool(poolName));
+    return new LoggerProxy(logger, {...options, poolName});
+  }
+}
+
+Loggers.define = function(loggerClassName, loggerClass) {
+  if(loggerClassName in loggerClasses) {
+    throw new Error(`Logger class ${loggerClassName} already exists.`);
+  }
+  if(typeof loggerClass !== 'function') {
+    throw new Error(`Invalid logger class ${loggerClassName}.`);
+  }
+  loggerClasses[loggerClassName] = loggerClass;
+};
+
+module.exports = { Loggers, LoggersPool, LoggerProxy, LoggerConsole };
--- utils/LoggerProxy.js
+++ utils/LoggerProxy.js
@@ -1,4 +1,4 @@
-
+const {Profiler} = require('@goldix.org/profiler');
 
 class LoggerProxy {
   constructor(logger, options) {
@@ -9,6 +9,9 @@ class LoggerProxy {
   }
   
   _log(level, message, payload, options) {
+    if(!this.logger) {
+      return Promise.resolve();
+    }
     return this.logger._log(
       level, message, payload, {...this.options, ...options}
     );
@@ -30,8 +33,28 @@ class LoggerProxy {
     return this._log('error', message, payload, options);
   }
   
+  getProfiler({options} = {}) {
+    return (options && options.Profiler) ||  Profiler;
+  }
+  
   start(message, payload, options) {
-    return this.logger.start(message, payload, {...this.options, ...options});
+    options = {...this.options, ...options};
+    
+    if(!this.logger) {
+      let Profiler = this.getProfiler(options);
+      return new Profiler({
+        onProfilerEnd: (duration, profiler, endArguments) => {
+          let [message2, payload2, options2] = endArguments || [];
+          return this._log(
+            'profiler',
+            message2 || message,
+            {...payload, ...payload2, duration},
+            {...options, options2, profiler }
+          );
+        },
+      });
+    }
+    return this.logger.start(message, payload, options);
   }
 }
 
